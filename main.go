b3d42b5a78e18f3e53522c35b4ae72cf0c337d82
Vlad Krapivin <vladkrapivin1995@gmail.com>
2018-05-22 21:55:18 +0300

    multipart upload;forms

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-// Ability to accept multiple files.
-// Ability to accept multipart/form-data requests.
 // Ability to accept JSON requests with BASE64 encoded images.
 // Ability to upload image by its URL (hosted somewhere in Internet).
 // Create thumb square preview (100 x 100 px) for every uploaded image.
@@ -14,23 +12,26 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"net/http"
-
-	"github.com/gorilla/mux"
 	"io"
 	"log"
+	"net/http"
 	"os"
+
+	"mime/multipart"
+
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
-const maxUploadSize = 2 * 1024 // 2 mb
-const imagesDir = "images"
+const (
+	maxUploadSize = 2 * 1024 // 2 mb
+	imagesDir     = "images"
+)
 
 func main() {
 	router := mux.NewRouter()
-
-	fs := http.FileServer(http.Dir(imagesDir))
-	router.Handle("/files/", http.StripPrefix("/files", fs))
-	router.HandleFunc("/", handler)
+	router.HandleFunc("/", upload)
 
 	server := http.Server{
 		Addr:    "0.0.0.0:8080",
@@ -40,58 +41,80 @@ func main() {
 	server.ListenAndServe()
 }
 
+// html upload page
 func display(writer http.ResponseWriter, data interface{}, filename string) {
 	file := fmt.Sprintf("%s.html", filename)
-	templates := template.Must(template.ParseFiles(file))
-	templates.ExecuteTemplate(writer, "layout", data)
+	t := template.Must(template.ParseFiles(file))
+	t.ExecuteTemplate(writer, "layout", data)
 }
 
-//This is where the action happens.
-func handler(w http.ResponseWriter, r *http.Request) {
+func upload(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	//GET displays the upload form.
 	case "GET":
-		display(w, "Add files", "index")
+		display(w, "Upload files", "index")
 
 		//POST takes the uploaded file(s) and saves it to disk.
 	case "POST":
-		//parse the multipart form in the request
-		err := r.ParseMultipartForm(maxUploadSize)
+		contentType := r.Header.Get("Content-Type")
+		// multipart data upload
+		if strings.HasPrefix(contentType, "multipart/form-data") {
+			uploadMultipart(w, r)
+		} else if contentType == "application/x-www-form-urlencoded" {
+			//uploadByLink(w,r)
+		} else {
+			fmt.Println(contentType)
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func uploadMultipart(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(maxUploadSize)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	mf := r.MultipartForm
+	//get the *fileheaders
+	files := mf.File["files"]
+	for _, file := range files {
+		//for each fileheader, get a handle to the actual file
+		err := saveFile(file)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
-		//get a ref to the parsed multipart form
-		m := r.MultipartForm
-
-		//get the *fileheaders
-		files := m.File["files"]
-		for i := range files {
-			//for each fileheader, get a handle to the actual file
-			file, err := files[i].Open()
-			defer file.Close()
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			//create destination file making sure the path is writeable.
-			filename := fmt.Sprintf("%s/%s", imagesDir, files[i].Filename)
-			dst, err := os.Create(filename)
-			defer dst.Close()
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			//copy the uploaded file to the destination file
-			if _, err := io.Copy(dst, file); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-		}
-		//display success message.
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
+	////display success message.
+	display(w, "Success!", "index")
+}
+
+func saveFile(fh *multipart.FileHeader) error {
+	mimeType := detectContentType(fh)
+	if mimeType != "image/jpeg" && mimeType != "image/png" && mimeType != "image/gif" {
+		return fmt.Errorf("file should be an image: got %s", mimeType)
+	}
+	file, err := fh.Open()
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	//create destination file making sure the path is write able.
+	filename := fmt.Sprintf("%s/%s", imagesDir, fh.Filename)
+	dst, err := os.Create(filename)
+	defer dst.Close()
+	if err != nil {
+		return err
+	}
+	//copy the uploaded file to the destination file
+	if _, err := io.Copy(dst, file); err != nil {
+		return err
+	}
+	return nil
+}
+
+func detectContentType(fh *multipart.FileHeader) string {
+	return fh.Header.Get("Content-Type")
 }
